4bc9bc0cabc3659740e33c2631d132128f702f6b
Fastrok <32041982+Fastrok@users.noreply.github.com>
2017-10-04 22:25:50 +0300

    Create phone.php

---
+++ phone.php
@@ -0,0 +1,32 @@
+<?php
+$peopleJson = file_get_contents(__DIR__.'/phone_book.json');
+$people = json_decode ($peopleJson, JSON_OBJECT_AS_ARRAY);
+?>
+<! DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>List</title>
+</head>
+<body>
+<table style="border: 1px solid black">
+    <tr>
+        <td> </td>
+        <td>   </td>
+        <td> </td>
+
+
+    </tr>
+
+    <?php foreach ($people as $key => $value) {?>
+        <tr>
+            <td><?= $key ?></td>
+            <td><?php echo $value ?></td>
+        </tr>
+    <?php } ?>
+
+
+
+</table>
+</body>
+</html>
